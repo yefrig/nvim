79f1a7b99f1cf1b5af876d55685dc33773ab22d0
Yefri Gaitan <yefrigaitan@gmail.com>
2024-05-04 17:41:33 -0400

    add java support

--- lua/community.lua
+++ lua/community.lua
@@ -6,5 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.java" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
 }
---
+++ lua/plugins/java.lua
@@ -0,0 +1,14 @@
+return {
+  "mfussenegger/nvim-jdtls",
+  opts = {
+    settings = {
+      format = {
+        enabled = true,
+        settings = {
+          url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+          profile = "GoogleStyle",
+        },
+      },
+    },
+  },
+}
