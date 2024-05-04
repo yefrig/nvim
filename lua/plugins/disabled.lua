54048019f2c700832ff7dcc43437c122f1cf89ce
Yefri Gaitan <yefrigaitan@gmail.com>
2024-05-04 13:50:41 -0400

    disable unneeded plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "akinsho/toggleterm.nvim", enabled = false },
+  { "stevearc/aerial.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "NvChad/nvim-colorizer.lua", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "s1n7ax/nvim-window-picker", enabled = false },
+}
